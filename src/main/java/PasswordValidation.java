0c06c03ca8d13864f57a62a50959adb71f05d549
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-02 10:42:56 +0200

    Update a file name and add methods for password validation

---
+++ src/main/java/PasswordValidation.java
@@ -0,0 +1,22 @@
+public class PasswordValidation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static boolean hasAtLeastEightCharacters () {
+        return false;
+    }
+
+    public static boolean containsDigits () {
+        return false;
+    }
+
+    public static boolean hasUpperAndLowercase () {
+        return false;
+    }
+
+    public static boolean detectCommonPasswords () {
+        return false;
+    }
+}
